35c02d40aa8b37ac4f6d836b63922064ecf34bf8
akatsuura <a.katsuura1991@gmail.com>
2016-04-03 11:47:29 +0900

    トップページをanswersにして表示する情報の整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :answers
   devise_for :users
-  get 'home/index'
-  root 'home#index'
+  get 'answers/index'
+  root 'answers#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
